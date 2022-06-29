eeb1cc0c50e7a9031ebea26ee5d8fdb5b45da597
ekrusznis <ekrusznis@gmail.com>
2022-06-29 07:49:42 -0400

    addded component -- ek

--- honeycomb/client/src/App.js
+++ honeycomb/client/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import navbar from './components/navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+      <navbar />
+
+//    <div className="App">
+//      <header className="App-header">
+//      </header>
+//    </div>
   );
 }
 
