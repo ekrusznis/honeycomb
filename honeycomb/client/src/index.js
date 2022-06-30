5df9d74d567f42e425ac19de7f6486f7e1cf6808
ekrusznis <ekrusznis@gmail.com>
2022-06-29 23:35:06 -0400

    figure out the issue with the theme-color

--- honeycomb/client/src/index.js
+++ honeycomb/client/src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Navigate, Router } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import "./assets/css/nucleo-icons.css";
+import "./assets/scss/blk-design-system-react.scss?v=1.2.0";
+import "./assets/demo/demo.css";
+
+import Index from "./views/Index.js";
+import LandingPage from "./views/examples/LandingPage.js";
+import RegisterPage from "./views/examples/RegisterPage.js";
+import ProfilePage from "./views/examples/ProfilePage.js";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Router>
+      <Route path="/components" render={(props) => <Index {...props} />} />
+      <Route
+        path="/landing-page"
+        render={(props) => <LandingPage {...props} />}
+      />
+      <Route
+        path="/register-page"
+        render={(props) => <RegisterPage {...props} />}
+      />
+      <Route
+        path="/profile-page"
+        render={(props) => <ProfilePage {...props} />}
+      />
+      <Navigate from="/" to="/components" />
+    </Router>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
